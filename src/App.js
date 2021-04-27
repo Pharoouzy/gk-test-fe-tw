7a4db6656cb6c8d7d945df7db175cd3e2c4f7c2a
Pharoouzy <pharoouzy@yahoo.com>
2021-04-27 13:35:33 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// import { useState, useEffect } from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  // Polyline,
+  // MarkerWithLabel,
+} from "react-google-maps";
+
 import './App.css';
+
+function Map () {
+  return (
+    <GoogleMap
+      defaultZoom={18}
+      defaultCenter={{ lat: 6.4937861, lng: 3.5840516 }}
+    >
+      <Marker
+        position={{ lat: 6.4937861, lng: 3.5840516 }}
+        icon={{
+          url: '/logo192.png',
+          scaledSize: new window.google.maps.Size(25, 25)
+        }}
+      />
+    </GoogleMap>
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <WrappedMap
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDy_0KGBQQWlCfn7JjOzXzk6eQfaF5IZS8&v=3.exp&libraries=geometry,drawing,places"
+        loadingElement={<div style={{ height: '100%' }} />}
+        containerElement={<div style={{ height: '100%' }} />}
+        mapElement={<div style={{ height: '100%' }} />}
+      />
     </div>
   );
 }
