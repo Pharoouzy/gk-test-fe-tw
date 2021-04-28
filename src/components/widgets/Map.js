c86f94f2f0a8b7e248ab70f799e2702533fa11c2
Pharoouzy <pharoouzy@yahoo.com>
2021-04-28 13:56:01 +0100

    update user current location

--- src/components/widgets/Map.js
+++ src/components/widgets/Map.js
@@ -7,7 +7,7 @@ import {
   // Polyline,
   // MarkerWithLabel,
 } from "react-google-maps";
-import mapStyles from "./mapStyles";
+import mapStyles from "../../mapStyles";
 import "./App.css";
 
 function Map() {
@@ -28,7 +28,7 @@ function Map() {
 
   return (
     <GoogleMap
-      defaultZoom={5}
+      defaultZoom={18}
       defaultCenter={{ lat: latValue, lng: lngValue }}
       defaultOptions={{ styles: mapStyles }}
     >
@@ -47,34 +47,11 @@ const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <div className="">
-        <h1>Parcel Request</h1>
-        <div className="flex space-x-8">
-          <div className="flex flex-col space-y-2 w-full">
-            <input
-              className="h-12 border border-k3gray px-3"
-              type
-              placeholder="Pickup Address"
-            />
-          </div>
-        </div>
-        <div className="flex space-x-8">
-          <input
-            className="h-12 border border-k3gray px-3"
-            type
-            placeholder="Dropoff Address"
-          />
-        </div>
-      </div>
-      <WrappedMap
+    <WrappedMap
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyDy_0KGBQQWlCfn7JjOzXzk6eQfaF5IZS8&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: "100%" }} />}
         containerElement={<div style={{ height: "100%" }} />}
         mapElement={<div style={{ height: "100%" }} />}
-      />
-    </div>
+    />
   );
 }
-
-export default App;
